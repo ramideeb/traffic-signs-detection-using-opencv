4082d66199fa685041686413ce36cc8134b057c1
ramideeb <35407681+ramideeb@users.noreply.github.com>
2019-05-03 15:21:59 +0300

    bugs fixes

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import cv2 as cv
 import numpy as np
 
 if __name__ == '__main__':
-    imageA = cv.imread("circles/stop.jpg")
+    imageA = cv.imread("")
+    #rescaling the image
+    W = 300.
+    height, width, depth = imageA.shape
+    imgScale = W / width
+    newX, newY = imageA.shape[1] * imgScale, imageA.shape[0] * imgScale
+    imageA = cv.resize(imageA, (int(newX), int(newY)))
+    #convert image to gray and hsv color spaces
     grayA = cv.cvtColor(imageA, cv.COLOR_BGR2GRAY)
     hsv=cv.cvtColor(imageA, cv.COLOR_BGR2HSV)
 
-    # cv.imshow("d", imageA)
-    # cv.waitKey(0)
-
     ##yellow detiction
-    lower_yellow=np.array([20, 100, 100])
-    upper_yellow=np.array([30, 255, 255])
+    lower_yellow=np.array([15, 100, 100])
+    upper_yellow=np.array([35, 255, 255])
     mask=cv.inRange(hsv,lower_yellow,upper_yellow)
     px=mask.shape[0]*mask.shape[1]
     whitey= cv.countNonZero(mask)
@@ -41,13 +45,14 @@ if __name__ == '__main__':
     whiter= cv.countNonZero(mask6)
 
     ##blue detiction
-    lower_blue=np.array([85,50,50])
-    upper_blue=np.array([125,255,255])
+    lower_blue=np.array([80,50,50])
+    upper_blue=np.array([130,255,255])
     mask7=cv.inRange(hsv,lower_blue,upper_blue)
     px7=mask.shape[0]*mask.shape[1]
     whiteblue= cv.countNonZero(mask7)
 
-#detect by color
+
+    #detected  by  color
     if whitey/px > 0.25 :
         print("exit")
     elif whiteg/px2 >0.2 :
@@ -59,30 +64,32 @@ if __name__ == '__main__':
     elif whiteblue/px > 0.2 and whiter/px > 0.2:
         print("park")
     elif  whiteblue/px <0.07 and  whiter/px<0.07 :
+        #to detect the images whis not containing blue and rde
         thresholded = cv.adaptiveThreshold(grayA, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
         kernel = np.ones((6, 6), np.uint8)
         erosion = cv.dilate(thresholded, kernel, iterations=1)
-        cv.imshow("d",erosion)
-        cv.waitKey(0)
+
+        # cv.imshow("d",erosion)
+        # cv.waitKey(0)
+
         edged = cv.Canny(erosion, 255, 255, 255)
         contours, hierarchy = cv.findContours(edged, cv.RETR_EXTERNAL,  cv.CHAIN_APPROX_TC89_L1)
         cv.drawContours(imageA, contours, -1, (0,255,0), 3)
+
         if ( len(contours)>11 ):
             print("endspeed limit")
         else:
             print("local destination")
+
     else:
+        #find contours
         ret, thresh1 = cv.threshold(grayA, 127, 255, cv.THRESH_BINARY)
         blur=cv.medianBlur(grayA,7)
         laplacian = cv.Laplacian(blur, cv.CV_8UC1)
-        # cv.imshow("d", laplacian)
-        # cv.waitKey(0)
         contours, hierarchy = cv.findContours(laplacian, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
         c = max(contours, key=cv.contourArea)
         epsilon = 0.02 * cv.arcLength(c , True)
         approx = cv.approxPolyDP(c , epsilon, True)
-
-        # print(len(approx))
 
         if(len(approx)==4):
             print("free road")
@@ -95,33 +102,34 @@ if __name__ == '__main__':
             mask = np.zeros((h + 2, w + 2), np.uint8)
             cv.floodFill(im_floodfill, mask, (0, 0), 255);
             im_floodfill_inv = cv.bitwise_not(im_floodfill)
+
             im_out = cimg | im_floodfill_inv
             for i in range(0, imageA.shape[0]):
                 for j in range(0, imageA.shape[1]):
                     if(im_out[i, j][0]!=255 or im_out[i, j][1]!=255  or im_out[i, j][2]!=255 ):
                         imageA[i, j] = 0
-
             # cv.imshow("d", imageA)
             # cv.waitKey(0)
+
             h, w = imageA.shape[:2]
             mask = np.zeros((h + 2, w + 2), np.uint8)
             # Floodfill from point (0, 0)
             cv.floodFill(imageA, mask, (0, 0), (255,255,255));
             grayB = cv.cvtColor(imageA, cv.COLOR_BGR2GRAY)
 
-            # cv.imshow("d", imageA)
+            # cv.imshow("ddddddddddddd", imageA)
             # cv.waitKey(0)
+            # print(len(approx))
+
+            #to detect traiangles
             if(len(approx)==3):
                 ret, thresh1 = cv.threshold(grayB, 127, 255, cv.THRESH_BINARY)
                 inv = cv.bitwise_not(thresh1)
-
                 blur = cv.medianBlur(grayB, 7)
                 laplacian = cv.Laplacian(blur, cv.CV_8UC1)
-
                 contours, hierarchy = cv.findContours(inv, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-                # print(len(contours))
 
-                ##yellow detiction
+                ##black detiction
                 lower_yellow = np.array([0, 0, 0])
                 upper_yellow = np.array([180, 255, 40])
                 hsv = cv.cvtColor(imageA, cv.COLOR_BGR2HSV)
@@ -138,50 +146,56 @@ if __name__ == '__main__':
                     print("BumbyRoad")
                 else:
                     print("give")
-
             else:
                 ret, thresh1 = cv.threshold(grayB, 127, 255, cv.THRESH_BINARY)
-                blur = cv.medianBlur(thresh1, 7)
-
+                blur = cv.medianBlur(thresh1, 11)
                 inv = cv.bitwise_not(blur)
-
-                cv.imshow("d", inv)
-                cv.waitKey(0)
-
                 contours, hierarchy = cv.findContours(inv, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
                 c = min(contours, key=cv.contourArea)
-
                 approx = cv.approxPolyDP(c, epsilon, True)
-                cv.drawContours(imageA,approx,-1,(0,255,0),4)
+
+                # cv.drawContours(imageA,approx,-1,(0,255,0),4)
+                # cv.imshow("d", imageA)
+                # cv.waitKey(0)
 
                 M = cv.moments(c)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
+                cX = int(M["m10"] / (M["m00"]+1))
+                cY = int(M["m01"] /( M["m00"]+1))
 
-                cv.circle(imageA, (cX, cY), 3, (0, 0, 0), -1)
+                # cv.circle(imageA, (cX, cY), 3, (0, 0, 0), -1)
+                # cv.imshow("d", imageA)
+                # cv.waitKey(0)
 
-                cv.imshow("d", imageA)
-                cv.waitKey(0)
+                #to count nuber of points on right and left and top
                 count=0
                 count2=0
+                count3=0
+                print(cY)
                 for i in range(len(approx)):
-                   if approx[i][0][0]> cX + imageA.shape[0]/10:
+                   if approx[i][0][0]> cX:
                        count=count+1
-                   elif approx[i][0][0] < cX + imageA.shape[0]/10 :
+                   if approx[i][0][0] < cX:
                        count2=count2+1
+                   if  approx[i][0][1] < cY:
+                       count3=count3+1
 
-
+                #to detect blue curciles
                 if(whiteblue/px7>0.3):
-                    if(len(contours)>3):
+                    if(len(contours)>4):
+                        # print(len(contours))
                         print("cannot be recognized")
                     else:
-                        if count == 3:
-                            print("right")
-                        elif count2 == 3:
-                            print("left")
-                        else:
+                        # print(count)
+                        # print(count3)
+                        if count3>count2 and count3>count:
                             print("up")
+                        elif count > count2:
+                            print("right")
+                        elif count2 >count:
+                            print("left")
+                # to detect red ones
                 else:
+                    # print(len(approx))
                     if len(approx)==4:
                         print("no")
                     else:
